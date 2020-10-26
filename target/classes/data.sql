160bf0042c61ade29e8c545f900c8533849d7c7f
javierzlafe <jig52646783595>
2020-10-26 10:53:26 -0300

    wip

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,8 @@
 DELETE FROM MOTORBIKE;
 
 INSERT INTO MOTORBIKE (id, model, release_year, brand, chassis, potence, torque, wheel_size, gear, usd_price) VALUES
-  (1, 'FZ-S FI', '2020', 'YAMAHA', 'chassis N/A', 0,0.0,0,0,1000.00),
-  (2, 'Boulevard M109', '2019', 'SUZUKI', 'chassis N/A', 0,0.0,0,0,1000.00),
-  (3, 'SUPERLEGGERA V4', '2018', 'DUCATI', 'chassis N/A', 0, 0.0, 0, 0, 1000.00);
+  (1, 'FZ-S FI', '2020', 'YAMAHA', 'mono-rigido', 17,12.3,17,5,2.965),
+  (2, 'Gsx 1000r', '2019', 'SUZUKI', 'multitubular', 202,178.5,17,6,48.000),
+  (3, 'SUPERLEGGERA V4', '2018', 'DUCATI', 'multitubular', 224, 186.4, 17, 6, 52.000),
+  (4, 'R1', '2020', 'YAMAHA' , 'diamante', 200,157.6,17,6, 44.000),
+  (5, '1000rr', '2020', 'BMW', 'diamante', 212,113.2,17,6,57.000);
